1c34a44814ad6f3d12089f2f4a295e79659f842e
ultramen <beranihjrah1@gmail.com>
2023-09-24 18:02:01 +0700

    Delete gemilang directory (dummy )

--- gemilang/function/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$server = "localhost";
-	$username = "id20834836_sayabang";
-	$password = "Manager44$";
-	$database = "id20834836_gemilang";
-	
-	$koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal");
